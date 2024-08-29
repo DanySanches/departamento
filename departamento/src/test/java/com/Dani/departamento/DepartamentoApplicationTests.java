1106157ec705ad39b7257532a9fe6609977ec466
danysanches@msn.com <danysanches@msn.com>
2024-08-28 22:29:00 -0300

    Initial commit

---
+++ departamento/src/test/java/com/Dani/departamento/DepartamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dani.departamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
